bc80b3c613d517165e919a6c2ab980c390194881
Shouvik Dutta <shouvik.dutta96@gmail.com>
2025-09-01 11:41:04 -0500

    minor updates

--- Azure/init.sh
+++ Azure/init.sh
@@ -12,12 +12,20 @@ sudo apt-get install -y docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Create a new directory
-cd $HOME
-mkdir backend
+# Create a all needed directories
+cd /home/azureuser
+mkdir -p backend/nginx/conf.d
+
+# Download nginx config file
+cd backend/nginx/conf.d
+wget https://raw.githubusercontent.com/sdutta9/docker_backend_apps/main/Azure/nginx/conf.d/httpbin.shouvik.dev.conf
+wget https://raw.githubusercontent.com/sdutta9/docker_backend_apps/main/Azure/nginx/conf.d/juice.shouvik.dev.conf
+wget https://raw.githubusercontent.com/sdutta9/docker_backend_apps/main/Azure/nginx/conf.d/vampi.shouvik.dev.conf
+wget https://raw.githubusercontent.com/sdutta9/docker_backend_apps/main/Azure/nginx/conf.d/upstreams.conf
+
 
 # Download docker compose file
-cd backend
+cd ../..
 wget https://raw.githubusercontent.com/sdutta9/docker_backend_apps/main/Azure/docker-compose.yaml
 
 # Run docker compose to create the containers
